64f4a25e907221be9cc3a2132b69537008a40ae2
OjOj717 <121485938+OjOj717@users.noreply.github.com>
2026-01-21 12:58:14 +0900

    Create 9095.cpp

---
+++ My Submission/9095.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    for (int j = 1; j <= t; ++j)
+    {
+        int n;
+        cin >> n;
+        
+        int arr[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
+        
+        for (int i = 4; i <= n; ++i) 
+        {
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        
+        cout << arr[n] << endl;
+    }
+    
+    return 0;
+}
