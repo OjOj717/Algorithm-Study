22839eb27c2da5d2acd56eb7b036bb9721ced3c4
OjOj717 <121485938+OjOj717@users.noreply.github.com>
2026-02-02 23:01:50 +0900

    26.02.02 10998

---
+++ My Submission/10998.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+long long a, b;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a >> b;
+
+    cout << a * b;
+
+    return 0;
+}
