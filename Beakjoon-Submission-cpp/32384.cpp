a0e4877fc17e54689a6de0c14015311ee9b39300
OjOj717 <121485938+OjOj717@users.noreply.github.com>
2026-02-06 23:18:32 +0900

    Create 32384.cpp

---
+++ Beakjoon-Submission-cpp/32384.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+long long n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    cout << "LoveisKoreaUniversity";
+    for (long long i = 1; i < n ; ++i)
+    {
+        cout << " LoveisKoreaUniversity";
+    }
+
+    return 0;
+}
